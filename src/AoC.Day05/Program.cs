2ead8f9dbf62f4ec4d7bd34cde58a9c32ec663b4
João Paulo <jpaulo_sanchez@hotmail.com>
2024-12-06 10:03:27 +0100

    day 5

---
+++ src/AoC.Day05/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+// Read the file
+//var stream = new StreamReader("..\\..\\..\\res\\sample01.txt");
+var stream = new StreamReader("..\\..\\..\\res\\input01.txt");
+
+RulesBuilder rulesBuilder = new();
+List<List<int>> updates = [];
+
+string? line;
+while ((line = stream.ReadLine()) != null)
+{
+    // part 1
+    if (string.IsNullOrWhiteSpace(line)) continue;
+
+    // first section
+    if (line.Contains('|')) rulesBuilder.Add((int.Parse(line.Split('|')[0]), int.Parse(line.Split('|')[1])));
+
+    // second section
+    else updates.Add(line.Split(',').Select(int.Parse).ToList());
+}
+
+// PART 1
+long sum = 0;
+
+Rules rules = rulesBuilder.Build();
+
+foreach (var update in updates)
+{
+    if (rules.AreRespectedBy(update))
+    {
+        sum += update[update.Count / 2];
+    }
+}
+
+Console.WriteLine($"sum is: {sum}");
+Console.WriteLine("Elapsed time: {0} ms", stopwatch.ElapsedMilliseconds);
+stopwatch.Restart();
+
+// PART 2
+sum = 0;
+
+foreach (var update in updates)
+{
+    if (rules.AreRespectedBy(update)) continue;
+
+    int notBefore, value;
+
+    while (!rules.AreRespectedBy(update, out notBefore, out value))
+    {
+        update.Remove(notBefore);
+        update.Insert(update.IndexOf(value), notBefore);
+    }
+
+    sum += update[update.Count / 2];
+}
+
+
+Console.WriteLine($"sum of corrected value is: {sum}");
+Console.WriteLine("Elapsed time: {0} ms", stopwatch.ElapsedMilliseconds);
+stopwatch.Stop();
---
+++ src/AoC.Day05/Rules.cs
@@ -0,0 +1,48 @@
+﻿global using Rule = (int beforethan, int value);
+
+internal record Rules(List<Rule> Value)
+{
+    public bool AreRespectedBy(List<int> values)
+    {
+        return AreRespectedBy(values, out _, out _);
+    }
+
+    public bool AreRespectedBy(List<int> values, out int notBefore, out int value)
+    {
+        notBefore = -1;
+        value = -1;
+
+        List<Rule> appliedRules = [];
+
+        foreach (var rule in Value)
+        {
+            if (values.Contains(rule.beforethan) && values.Contains(rule.value))
+            {
+                appliedRules.Add(rule);
+            }
+        }
+
+        foreach (var rule in appliedRules)
+        {
+            if (values.IndexOf(rule.beforethan) > values.IndexOf(rule.value))
+            {
+                notBefore = rule.beforethan;
+                value = rule.value;
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+internal class RulesBuilder
+{
+    private List<Rule> _rules = [];
+
+    public void Add(Rule rule) => _rules.Add(rule);
+
+    public Rules Build() => new(_rules);
+
+}
