ad053cb519cb8d96efb61884f966a0c800c3b957
João Paulo <jpaulo_sanchez@hotmail.com>
2024-12-06 20:29:13 +0100

    day 4

---
+++ src/AoC.Day04/Extensions.cs
@@ -0,0 +1,140 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC.Day04;
+
+internal static class Extensions
+{
+    public static long CountXMAS(this List<List<char>> chars)
+    {
+        long sum = 0;
+        foreach (var row in chars)
+        {
+            Regex regex = new Regex("(?=(XMAS|SAMX))");
+            sum += regex.Count(new string([.. row])!);
+        }
+
+        Console.WriteLine(sum);
+        return sum;
+    }
+
+    public static List<List<T>> Transpose<T>(this List<List<T>> input)
+    {
+        List<List<T>> output = [];
+        for (int i = 0; i < input[0].Count; i++)
+        {
+            List<T> row = [];
+            for (int j = 0; j < input.Count; j++)
+            {
+                row.Add(input[j][i]);
+            }
+            output.Add(row);
+        }
+        return output;
+    }
+
+    public static List<List<T>> MirrorVertical<T>(this List<List<T>> input)
+    {
+        List<List<T>> output = [];
+        for (int i = 0; i < input.Count; i++)
+        {
+            List<T> row = [];
+            for (int j = input[0].Count - 1; j >= 0; j--)
+            {
+                row.Add(input[i][j]);
+            }
+            output.Add(row);
+        }
+        return output;
+    }
+
+    public static List<List<T>> RotateMatrixBy45Degrees<T>(this List<List<T>> input)
+    {
+        List<List<T>> output = [];
+
+        // Rows
+        for (int j = 0; j < input[0].Count; j++)
+        {
+            List<T> row = [];
+            try
+            {
+                int a = 0;
+                int b = j;
+
+                while (true)
+                {
+                    row.Add(input[a][b]);
+
+                    a++;
+                    b--;
+                }
+            }
+            catch (ArgumentOutOfRangeException) { }
+
+            output.Add(row);
+        }
+
+        // Columns
+        for (int i = 1; i < input.Count; i++)
+        {
+            List<T> row = [];
+            try
+            {
+                int a = i;
+                int b = input[0].Count - 1;
+                while (true)
+                {
+                    row.Add(input[a][b]);
+                    a++;
+                    b--;
+                }
+            }
+            catch (ArgumentOutOfRangeException) { }
+            output.Add(row);
+        }
+
+        return output;
+    }
+
+
+    public static bool CheckX_MAS(this List<List<char>> map, (int i, int j) position)
+    {
+        try
+        {
+            // Check if the middle is A
+            if (map[position.i][position.j] != 'A') return false;
+
+            // Get diagonals, is in the try catch to avoid out of range
+            var (i, j) = position;
+            List<char> surroungingDiagonals = [
+                map[i - 1][j - 1],
+                map[i - 1][j + 1],
+                map[i + 1][j - 1],
+                map[i + 1][j + 1]];
+
+            // Remove the case
+            // M   S
+            //   A
+            // S   M
+            if (surroungingDiagonals[0] == surroungingDiagonals[3]) return false;
+
+            // Find if there is the same amount of M and S
+            int sum = 0;
+            foreach (var c in surroungingDiagonals)
+            {
+                sum += c switch
+                {
+                    'M' => +1,
+                    'S' => -1,
+                    _ => -100
+                };
+            }
+            if (sum == 0) return true;
+
+            return false;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return false;
+        }
+    }
+}
---
+++ src/AoC.Day04/Program.cs
@@ -0,0 +1,69 @@
+﻿using AoC.Day04;
+using System.Diagnostics;
+
+Stopwatch sw = new();
+sw.Start();
+
+//var stream = new StreamReader("..\\..\\..\\res\\sample01.txt");
+var stream = new StreamReader("..\\..\\..\\res\\input01.txt");
+
+
+long sum = 0;
+List<List<char>> chars = [];
+
+// Read the file
+string? line;
+while ((line = stream.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(line)) continue;
+
+    chars.Add([.. line.ToCharArray()]);
+}
+
+Console.WriteLine($"Read lines in {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+
+// PART 1
+//  Count the appearance of xmax in a axis
+Console.WriteLine("Horizontal");
+sum += chars.CountXMAS();
+
+// count the appearance of xmax in a vertical axis
+Console.WriteLine("\nVertical");
+sum += chars.Transpose().CountXMAS();
+
+
+// Count the appearance of xmax in first diagonal axis
+Console.WriteLine("\n/");
+sum += chars.RotateMatrixBy45Degrees().CountXMAS();
+
+
+// Count the appearance of xmax second diagonal axis
+Console.WriteLine("\n\\");
+sum += chars.MirrorVertical().RotateMatrixBy45Degrees().CountXMAS();
+
+
+
+Console.WriteLine($"Part 1: Sum of matches found: {sum}");
+Console.WriteLine($"Part 1: Elapsed time: {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+
+// Part 2
+sum = 0;
+
+for (int i = 0; i < chars[0].Count; i++)
+{
+    for (int j = 0; j < chars.Count; j++)
+    {
+
+        if (chars.CheckX_MAS((i, j))) sum++;
+    }
+}
+
+Console.WriteLine($"Part 2: Sum of matches found: {sum}");
+Console.WriteLine($"Part 2: Elapsed time: {sw.ElapsedMilliseconds}ms");
+sw.Stop();
+
+
+
+
