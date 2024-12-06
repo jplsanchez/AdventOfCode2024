71507ea33e691b5d94b676fc4f0922f648ec6007
João Paulo <jpaulo_sanchez@hotmail.com>
2024-12-06 15:01:18 +0100

    day 6

---
+++ src/AoC.Day06/Guard.cs
@@ -0,0 +1,50 @@
+﻿global using Position = (int x, int y);
+
+internal enum Orientation
+{
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+}
+
+internal class Guard(Position position, Orientation orientation)
+{
+    public Position Position { get; set; } = position;
+    public Orientation Orientation { get; set; } = orientation;
+
+    public static bool operator ==(Guard left, Guard right) => left.Position.x == right.Position.x && left.Position.y == right.Position.y && left.Orientation == right.Orientation;
+    public static bool operator !=(Guard left, Guard right) => !(left == right);
+    public override bool Equals(object? obj) => obj is Guard guard && this == guard;
+    public override int GetHashCode() => base.GetHashCode();
+
+    public Position NextStep() => Orientation switch
+    {
+        Orientation.UP => (Position.x, Position.y - 1),
+        Orientation.RIGHT => (Position.x + 1, Position.y),
+        Orientation.DOWN => (Position.x, Position.y + 1),
+        Orientation.LEFT => (Position.x - 1, Position.y),
+        _ => throw new ArgumentOutOfRangeException(),
+    };
+
+    public void TurnRight() => Orientation = Orientation switch
+    {
+        Orientation.UP => Orientation.RIGHT,
+        Orientation.RIGHT => Orientation.DOWN,
+        Orientation.DOWN => Orientation.LEFT,
+        Orientation.LEFT => Orientation.UP,
+        _ => throw new ArgumentOutOfRangeException(),
+    };
+
+    public void Move() => Position = NextStep();
+
+    public Guard DeepCopy() => new(Position, Orientation);
+}
+
+
+internal readonly record struct PositionLog(Position Position, Orientation Orientation)
+{
+    public override int GetHashCode() => HashCode.Combine(Position.x, Position.y, Orientation);
+
+    public static PositionLog LogFrom(Guard guard) => new(guard.Position, guard.Orientation);
+};
---
+++ src/AoC.Day06/Program.cs
@@ -0,0 +1,81 @@
+﻿using AoC.Day06;
+using System.Diagnostics;
+
+Stopwatch sw = new();
+sw.Start();
+
+// Read the file
+const bool IS_TEST = false;
+
+StreamReader stream = IS_TEST switch
+{
+    true => new("..\\..\\..\\res\\sample01.txt"),
+    _ => new("..\\..\\..\\res\\input01.txt"),
+};
+
+string? line;
+List<List<char>> map = [];
+Guard guard = new((0, 0), Orientation.UP);
+
+while ((line = stream.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(line)) continue;
+    if (line.Contains('^')) guard = new((line.IndexOf('^'), map.Count), Orientation.UP);
+    map.Add([.. line.ToCharArray()]);
+}
+
+List<List<char>> mapBackup = map.DeepCopy();
+Guard guardBackup = guard.DeepCopy();
+
+Console.WriteLine($"Map loaded in {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+
+
+// PART 1
+try
+{
+    while (true)
+    {
+        map[guard.Position.y][guard.Position.x] = 'X';
+        var next = guard.NextStep();
+        var nextChar = map[next.y][next.x];
+
+        if (nextChar == '#') guard.TurnRight();
+        guard.Move();
+    }
+}
+catch (ArgumentOutOfRangeException) { }
+
+long sum = 0;
+foreach (var row in map)
+{
+    sum += row.Count(c => c.Equals('X'));
+}
+
+Console.WriteLine($"Sum of tiles passed by the guard: {sum}");
+Console.WriteLine($"Part 1 ran in {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+
+// PART 2
+sum = 0;
+
+Parallel.For(0, map.Count, i =>
+{
+    var localMap = mapBackup.DeepCopy();
+    var localGuard = guardBackup.DeepCopy();
+
+    for (int j = 0; j < localMap[i].Count; j++)
+    {
+        if (localMap[i][j] == '#' || localMap[i][j] == '^') continue;
+        localMap[i][j] = 'O';
+
+        if (localMap.IsCircularPath(localGuard)) sum++;
+
+        localMap = mapBackup.DeepCopy();
+        localGuard = guardBackup.DeepCopy();
+    }
+});
+
+Console.WriteLine($"Number of obstructions: {sum}");
+Console.WriteLine($"Part 2 ran in {sw.ElapsedMilliseconds}ms");
+sw.Stop();
