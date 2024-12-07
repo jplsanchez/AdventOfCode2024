572a07877de2a8d045e00c5a457a20a5c7d9b9c5
João Paulo <jpaulo_sanchez@hotmail.com>
2024-12-07 10:34:38 +0100

    day 7

---
+++ src/AoC.Day07/Calculator.cs
@@ -0,0 +1,63 @@
+﻿
+//// PART 2
+//sum = 0;
+
+
+//Console.WriteLine($"Part 2 sum: {sum}");
+//Console.WriteLine($"Part 2 ran in {sw.ElapsedMilliseconds}ms");
+//sw.Stop();
+
+internal class Calculator(long Result, List<long> Numbers)
+{
+    public bool PossibleSolutionExists() => CheckRecursively(0, 0, Operator.Add) || CheckRecursively(0, 1, Operator.Multiply);
+
+    private bool CheckRecursively(int i, long acumulated, Operator operation)
+    {
+        if (i == Numbers.Count) return acumulated == Result;
+
+        acumulated = operation switch
+        {
+            Operator.Add => acumulated + Numbers[i],
+            Operator.Multiply => acumulated * Numbers[i],
+            _ => throw new NotImplementedException()
+        };
+
+        return CheckRecursively(i + 1, acumulated, Operator.Add) ||
+               CheckRecursively(i + 1, acumulated, Operator.Multiply);
+    }
+
+    private enum Operator
+    {
+        Add,
+        Multiply
+    }
+}
+
+internal class CalculatorExpanded(long Result, List<long> Numbers)
+{
+    public bool PossibleSolutionExists() => CheckRecursively(0, 0, Operator.Add) || CheckRecursively(0, 1, Operator.Multiply);
+
+    private bool CheckRecursively(int i, long acumulated, Operator operation)
+    {
+        if (i == Numbers.Count) return acumulated == Result;
+
+        acumulated = operation switch
+        {
+            Operator.Add => acumulated + Numbers[i],
+            Operator.Multiply => acumulated * Numbers[i],
+            Operator.Concatenate => long.Parse(acumulated.ToString() + Numbers[i].ToString()),
+            _ => throw new NotImplementedException()
+        };
+
+        return CheckRecursively(i + 1, acumulated, Operator.Add) ||
+               CheckRecursively(i + 1, acumulated, Operator.Multiply) ||
+               CheckRecursively(i + 1, acumulated, Operator.Concatenate);
+    }
+
+    private enum Operator
+    {
+        Add,
+        Multiply,
+        Concatenate
+    }
+}
---
+++ src/AoC.Day07/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+Stopwatch sw = new();
+sw.Start();
+
+// Read the file
+const bool IS_TEST = false;
+
+StreamReader stream = IS_TEST switch
+{
+    true => new("..\\..\\..\\res\\sample01.txt"),
+    _ => new("..\\..\\..\\res\\input01.txt"),
+};
+
+string? line;
+long sum = 0;
+long sum2 = 0;
+
+while ((line = stream.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(line)) continue;
+
+    // PART 1
+    long result = long.Parse(line.Split(':')[0]);
+    List<long> numbers = line.Split(' ').Select(long.Parse).Skip(1).ToList();
+
+
+    Calculator calculator = new(result, numbers);
+
+    if (calculator.PossibleSolutionExists())
+    {
+        sum += result;
+    }
+
+    // PART 2
+    CalculatorExpanded calculatorExpanded = new(result, numbers);
+
+    if (calculatorExpanded.PossibleSolutionExists())
+    {
+        sum2 += result;
+    }
+}
+
+Console.WriteLine($"Part 1 Sum: {sum}");
+Console.WriteLine($"Part 2 Sum: {sum2}");
+Console.WriteLine($"Eerything ran in {sw.ElapsedMilliseconds}ms");
+sw.Stop();
